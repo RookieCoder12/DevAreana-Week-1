a9e0cff8c6f0d9b566e817d11f51ea3f926b74da
Bipranshu Baghel <136240352+RookieCoder12@users.noreply.github.com>
2026-02-12 13:45:54 +0530

    completed personal_intro.py

---
+++ personal_intro.py
@@ -0,0 +1,6 @@
+name = str(input("What is your name?: "))
+age = int(input("How old are you?: "))
+hby = str(input("What is your favourite hobby?: "))
+
+print(f"\n🎉Welcome {name}🎉")
+print(f"You are {age} years old and love {hby}.")
